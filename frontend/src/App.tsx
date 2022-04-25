d01ff0b7295f57bb40eee710e7044299d166045f
Stephen Jayakar <aj12ay@gmail.com>
2022-04-24 17:21:27 -0700

    abstract workoutcard to its own component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import { useQuery, useMutation } from "../convex/_generated";
 
 import {
@@ -17,10 +15,11 @@ import {
   Paper,
 } from "@mui/material";
 
-import { Workout, WorkoutConvexResponse } from "./models";
+import { WorkoutConvexResponse } from "./models";
+import WorkoutCard from "./WorkoutCard";
 
 export default function App() {
-  // const insertWorkout = useMutation("insertWorkout");
+  // const insertWorkout = useMutation('insertWorkout');
   // TODO: Get the Convex Workout response instead
   const workouts: [WorkoutConvexResponse] = useQuery("getWorkouts") ?? [];
 
@@ -49,55 +48,5 @@ export default function App() {
         <WorkoutCard key={workout.date} workout={workout} />
       ))}
     </main>
-  );
-}
-
-function WorkoutCard(props: { key: string; workout: Workout }) {
-  const workout = props.workout;
-  const [showWorkout, setShowWorkout] = useState(false);
-
-  return (
-    <Card>
-      <Button
-        variant="contained"
-        onClick={() => {
-          setShowWorkout(!showWorkout);
-        }}
-      >
-        {(!showWorkout ? "Show workout " : "Hide workout ") + workout.date}
-      </Button>
-      {showWorkout ? <WorkoutTable workout={workout} /> : <div />}
-    </Card>
-  );
-}
-
-function WorkoutTable(props: { workout: Workout }) {
-  const workout: Workout = props.workout;
-  return (
-    <div className="WorkoutTable">
-      {workout.exercises.map((e) => (
-        <>
-          <h1>{e.exercise_name}</h1>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 200 }} aria-label="customized table">
-              <TableHead>
-                <TableRow>
-                  <TableCell align="right">Weight</TableCell>
-                  <TableCell align="right">Reps</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {e.sets.map((s, i) => (
-                  <TableRow key={e.exercise_name + i}>
-                    <TableCell align="right">{s.weight}</TableCell>
-                    <TableCell align="right">{s.reps}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </>
-      ))}
-    </div>
   );
 }
